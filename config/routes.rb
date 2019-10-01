30130bfcf27fd01a39c3319675f720e5de8a62bb
atamez31 <andres.96hdz@hotmail.com>
2019-10-01 00:41:28 -0500

    Agregar controllers al usuario y acomodar el front de las views nuevas

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
   devise_for :admins, path: 'admins', controllers: {
-    sessions: "admins/sessions"
+    sessions:           "admins/sessions",
+    passwords:          "admins/passwords",
+    registrations:      "admins/registrations",
+    confirmations:      "admins/confirmations"
+    # omniauth_callbacks: "users/omniauth_callbacks"
   }
   root to: 'pages#home'
-  devise_for :users
+  devise_for :users, path: 'users', controllers: {
+    sessions:           "users/sessions",
+    passwords:          "users/passwords",
+    registrations:      "users/registrations",
+    confirmations:      "users/confirmations"
+    # omniauth_callbacks: "users/omniauth_callbacks"
+      }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
