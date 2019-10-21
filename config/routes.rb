0e3f5043dcdbe2d16e80da9a81ef5041d43ec58c
martinc91 <martincc91@gmail.com>
2019-10-21 00:06:50 -0500

    commit antes de pull

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+ Rails.application.routes.draw do
   devise_for :admins, path: 'admins', controllers: {
     sessions:           "admins/sessions",
     passwords:          "admins/passwords",
