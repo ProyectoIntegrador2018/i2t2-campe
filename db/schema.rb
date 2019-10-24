b4c2bf653e6b0679e3725a4133af3644acf05050
Hal Perry Cardenas <hal96.perry@gmail.com>
2019-10-24 15:09:37 -0500

    Creation of groups models and models_users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_17_230734) do
+ActiveRecord::Schema.define(version: 2019_10_24_200507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 2019_10_17_230734) do
     t.boolean "is_admin"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "groups_users", id: false, force: :cascade do |t|
+    t.integer "group_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
