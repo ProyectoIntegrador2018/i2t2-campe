9445c1a1770c73e73aa2f4b0de66e3cefa5d8a3c
Hal96 <hal96.perry@gmail.com>
2019-10-24 19:24:38 -0500

    Model/convocatorias (#59)
    * Creation of groups models and models_users table
    
    * Creates models for groups and joint table for users
    
    * Fixes code climate issues
    
    * Fixes code climate issues
    
    * Fixes code climate issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
+# frozen_string_literal: true
+
+# La clase responsable  de user
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :groups_users
+  has_many :groups, through: :groups_users
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+      
          
   def update_imported_user(file)
     spreadsheet = open_spreadsheet(file)
