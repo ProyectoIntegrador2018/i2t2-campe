14074177aefd98f69e0bcb498a3d829418f79e3e
Hal Perry Cardenas <hal96.perry@gmail.com>
2019-10-24 19:08:32 -0500

    Fixes code climate issues

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,4 @@
+# Esta es la clase de convocatorias
 class Group < ApplicationRecord
   has_many :groups_users
   has_many :users, through: :groups_users
--- app/models/groups_user.rb
+++ app/models/groups_user.rb
@@ -1,3 +1,4 @@
+#Clase de  relacion m a n entre grupos y users
 class GroupsUser < ApplicationRecord
   belongs_to :group
   belongs_to :student
