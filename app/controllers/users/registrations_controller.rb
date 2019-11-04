632308f48f783f45496d2b6fd0780d67e2e88308
atamez31 <andres.96hdz@hotmail.com>
2019-11-04 11:32:26 -0600

    form de agregar usuarios con todos los campos

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,62 +1,21 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  protected
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:email, :desc_request_status, :cvu,
+                                               :name, :paternal_last_name, :maternal_last_name, :rfc,
+                                               :curp, :gender, :marital_stauts, :country_birth, :state_birth,
+                                               :street_address, :street_number_address_ext, :street_number_address_int,
+                                               :colony_address, :city_address, :municipiality_address, :state_address,
+                                               :phone_number, :cell_phone, :convocatory, :fiscal_year, :studies_start_date,
+                                               :studies_end_date, :start_scholarship, :end_scholarship, :school, :entity,
+                                               :support_to_get, :program, :expertise_area, :field_study, :discipline
+                                            ])
+  end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 end
