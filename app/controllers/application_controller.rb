84008315d909b094cc28a7db41e88830188c96f4
atamez31 <andres.96hdz@hotmail.com>
2019-11-06 18:00:47 -0600

    code climate fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,35 +1,4 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
-#   before_action :configure_permitted_parameters, if: :devise_controller?
-
-#   protected
-
-#   def configure_permitted_parameters
-#     devise_parameter_sanitizer.permit(:sign_up,
-#                                       keys: %i[:email :desc_request_status :cvu
-#                                                :name :paternal_last_name :maternal_last_name :rfc
-#                                                :curp :gender :marital_stauts :country_birth :state_birth
-#                                                :street_address :street_number_address_ext :street_number_address_int
-#                                                :colony_address :city_address :municipiality_address :state_address
-#                                                :phone_number :cell_phone :convocatory :fiscal_year :studies_start_date
-#                                                :studies_end_date :start_scholarship :end_scholarship :school :entity
-#                                                :support_to_get :program :expertise_area :field_study :discipline
-#                                                :sub_discipline :last_gpa :recovered_gpa :scholarchip_requested
-#                                                :scholarship_currency :maintenance_requested :maintenance_currency
-#                                                :medical_insurance_reuquested :medical_insurance_currency
-#                                             ])
-#     # devise_parameter_sanitizer.permit(:account_update,
-#     #                                   keys: %i[email desc_request_status cvu
-#     #                                            name paternal_last_name maternal_last_name rfc
-#     #                                            curp gender marital_stauts country_birth state_birth
-#     #                                            street_address street_number_address_ext street_number_address_int
-#     #                                            colony_address city_address municipiality_address state_address
-#     #                                            phone_number cell_phone convocatory fiscal_year studies_start_date
-#     #                                            studies_end_date start_scholarship end_scholarship school entity
-#     #                                            support_to_get program expertise_area field_study discipline
-#     #                                            sub_discipline last_gpa recovered_gpa scholarchip_requested
-#     #                                            scholarship_currency maintenance_requested maintenance_currency
-#     #                                            medical_insurance_reuquested medical_insurance_currency
-#     #                                         ])
-#   end
-
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,21 +2,31 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-
   protected
 
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:email, :desc_request_status, :cvu,
-                                               :name, :paternal_last_name, :maternal_last_name, :rfc, :birth_date,
-                                               :curp, :gender, :marital_stauts, :country_birth, :state_birth,
-                                               :street_address, :street_number_address_ext, :street_number_address_int,
-                                               :colony_address, :city_address, :municipiality_address, :state_address,
-                                               :phone_number, :cell_phone, :convocatory, :fiscal_year, :studies_start_date,
-                                               :studies_end_date, :start_scholarship, :end_scholarship, :school, :entity,
-                                               :support_to_get, :program, :expertise_area, :field_study, :discipline,
-                                               :sub_discipline, :last_gpa
-                                            ])
+                                      keys: [:email,
+                                             :desc_request_status,
+                                             :cvu, :name, :paternal_last_name,
+                                             :maternal_last_name,
+                                             :rfc, :birth_date, :curp,
+                                             :gender, :marital_stauts,
+                                             :country_birth, :state_birth,
+                                             :street_address,
+                                             :street_number_address_ext,
+                                             :street_number_address_int,
+                                             :colony_address, :city_address,
+                                             :municipiality_address,
+                                             :state_address, :phone_number,
+                                             :cell_phone, :convocatory,
+                                             :fiscal_year, :studies_start_date,
+                                             :studies_end_date,
+                                             :start_scholarship,
+                                             :end_scholarship, :school, :entity,
+                                             :support_to_get, :program,
+                                             :expertise_area, :field_study,
+                                             :discipline, :sub_discipline,
+                                             :last_gpa])
   end
-
 end
