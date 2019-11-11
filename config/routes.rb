bdd822f6cf01dcb3711a19ffdb05f7e542d19041
IvanVazquezL <ivanvl_11@hotmail.com>
2019-11-10 21:08:01 -0600

    dashboard added

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
     # omniauth_callbacks: "users/omniauth_callbacks"
       }
   resources :users, only: [:new, :create, :show, :index, :destroy]
+  resources :pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
