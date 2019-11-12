dd9e4278cfdcbf59bc5da1158dcc7062c07fb97b
IvanVazquezL <ivanvl_11@hotmail.com>
2019-11-12 14:57:31 -0600

    errors fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#This class allows the sign in path
+# This class allows the sign in path
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(_resource)
     '/pages'
