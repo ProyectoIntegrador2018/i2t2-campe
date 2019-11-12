30eaf3dd8f8397760ad3deab5687d2d9dbda0682
Hal96 <hal96.perry@gmail.com>
2019-11-12 16:16:21 -0600

    Delets user (#66)
    * Delets user
    
    * Fixes delete issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,13 @@ class UsersController < ApplicationController
 
   def export_user
   end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
   private
   def clean(headers)
     (0..headers.length).each do |i|
@@ -84,4 +91,12 @@ class UsersController < ApplicationController
     else raise "Unknown file type: #{file.original_filename}"
     end
   end
+
+  def user_params
+    params.require(:user).permit(:name, :paternal_last_name, :maternal_last_name, :email, :password, :password_confirmation, :request_password_it2t2, :creation_date, :desc_request_status, :cvu,  
+    :rfc, :curp, :gender, :marital_stauts, :birth_date, :country_birth, :state_birth, :street_address, :street_number_address_ext, :street_number_address_int, :colony_address, :city_address, :municipiality_address,
+    :state_address, :phone_number, :cell_phone, :convocatory, :fiscal_year, :studies_start_date, :studies_end_date, :start_scholarship, :end_scholarship, :school, :entity, :support_to_get, :program, :expertise_area,
+    :field_study, :discipline, :sub_discipline, :last_gpa)
+  end
+
 end
