60faf0fa000327e347a29f252e8eaabffd2aa4f4
IvanVazquezL <ivanvl_11@hotmail.com>
2019-11-12 15:13:21 -0600

    Front e (#63)
    * dashboard added
    
    * errors fixed
    
    * errors fixed
    
    * errors fixed
    
    * errors fixed
    
    * errors fixed
    
    * errors fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
+# This class allows the sign in path
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(_resource)
+    '/pages'
+  end
 end
