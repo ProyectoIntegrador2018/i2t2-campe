cd6e03624f6c6980ea7311f566a8c09d78ff5fb9
Hal96 <hal96.perry@gmail.com>
2019-11-19 15:06:09 -0600

    Creation of groups

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,6 @@ Rails.application.routes.draw do
       }
   resources :users, only: [:new, :create, :show, :index, :destroy]
   resources :pages
+  resources :groups
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
