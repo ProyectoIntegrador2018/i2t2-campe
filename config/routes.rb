56e9421bb22517d0729cf08a7793f6df3b8b189a
IvanVazquezL <ivanvl_11@hotmail.com>
2019-11-19 20:54:25 -0600

    show users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
   devise_for :admins, path: 'admins', controllers: {
     sessions:           "admins/sessions",
     passwords:          "admins/passwords",
@@ -7,16 +6,16 @@ Rails.application.routes.draw do
     confirmations:      "admins/confirmations"
     # omniauth_callbacks: "users/omniauth_callbacks"
   }
-  root to: 'pages#home'
-  get 'export_users' => 'users#export_user'
-  post 'import_users' => 'users#import_user', as: 'import_users'
   devise_for :users, path: 'users', controllers: {
     sessions:           "users/sessions",
     passwords:          "users/passwords",
     registrations:      "users/registrations",
     confirmations:      "users/confirmations"
     # omniauth_callbacks: "users/omniauth_callbacks"
-      }
+  }
+  root to: 'pages#home'
+  get 'export_users' => 'users#export_user'
+  post 'import_users' => 'users#import_user', as: 'import_users'
   resources :users, only: [:new, :create, :show, :index, :destroy]
   resources :pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
