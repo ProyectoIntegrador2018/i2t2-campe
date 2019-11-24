ed69a1cf5cddb98f97f0d7f89cca115086f1f2ce
Hal96 <hal96.perry@gmail.com>
2019-11-24 17:42:02 -0600

    code climate fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,16 +9,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def import_user
-    file = params[:file]
-    file_type = ile_type = file.present? ? file.original_filename.split('.').last.to_s.downcase : ''
-    if file.present? and (file_type == 'csv' or file_type == 'xlsx' or file_type == 'xls')
-      update_imported_user(file)
-    else
-      redirect_to users_path, flash: {danger: 'csv'} 
-    end
-  end
-
   def destroy
     @user = User.find(params[:id])
     @user.destroy
@@ -32,52 +22,6 @@ class UsersController < ApplicationController
     headers
   end
   
-  def update_imported_user(file)
-    spreadsheet = open_spreadsheet(file)
-    header = spreadsheet.row(1)
-    (2..spreadsheet.last_row).each do |i|
-      row = Hash[[header, spreadsheet.row(i)].transpose]
-      # @store = Store.new(row)
-      @user = User.new(
-        email: row['CORREO'],
-        :password => '111111', 
-        :password_confirmation => '111111', 
-        request_password_it2t2: row['CLAVESOLICITUD'],
-        creation_date: row['FECHA'+'\n'+'CREACION'], 
-        desc_request_status: row['DESCESTATUSSOLICITUD'], 
-        cvu: row['CVU'], name: row['NOMBRE'], 
-        paternal_last_name: row['APELLIDOPATERNO'],
-        maternal_last_name: row['APELLIDOMATERNO'], 
-        rfc: row['RFC'], curp: row['CURP'], 
-        gender: row['GÉNERO'], marital_stauts: row['ESTADOCIVIL'], 
-        birth_date: row['FECHANACIMIENTO'], 
-        country_birth: row['ESTADONACIMIENTO'], 
-        street_address: row['DOMICILIOCALLE'], 
-        street_number_address_ext: row['DOMICILIONUM.EXT.'], 
-        street_number_address_int: row['DOMICILIONUM.INT.'],
-        colony_address: row['DOMICILIOCOLONIA'], 
-        city_address: row['DOMICILIOCIUDAD'], 
-        municipiality_address: row['DOMICILIOMUNICIPIO'], 
-        state_address: row['DOMICILIOESTADO'],
-        phone_number: row['TELÉFONO'], 
-        cell_phone: row['CELULAR'], 
-        studies_start_date: row['INICIODEESTUDIOS'], 
-        studies_end_date: row['FINDEESTUDIOS'], 
-        start_scholarship: row['INICIODEBECA'], 
-        end_scholarship: row['FINDEBECA'], 
-        school: row['INSTITUCIÓN'], 
-        entity: row['ENTIDAD'], 
-        support_to_get: row['APOYOAOBTENER'], 
-        program: row['PROGRAMA'], 
-        expertise_area: row['ÁREADELCONOCIMIENTO'],
-        field_study: row['CAMPO'], 
-        discipline: row['DISCIPLINA'], 
-        sub_discipline: row['SUBDISCIPLINA'], 
-        last_gpa: row['PROMEDIOÚLTIMOGRADO']
-      )
-      @user.save
-    end
-    redirect_to users_path
   end
 
   def open_spreadsheet(file)
