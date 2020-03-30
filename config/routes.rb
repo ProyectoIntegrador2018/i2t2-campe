cdf4432cad0de962f94465d0bf46b72c4e10b6f8
Rene Garcia <rene.saaenz@gmail.com>
2020-03-30 17:04:42 -0600

    Renames UsersController to StudentsController

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'export_users' => 'users#export_user'
   post 'import_users' => 'users#import_user', as: 'import_users'
-  resources :users, only: [:new, :create, :show, :index, :destroy]
+  resources :students
   resources :pages
   resources :groups 
   resources :groups_users
