4b2fe4f45e26d195063bd970d5a062aec7b50e71
Eduardo Trujillo <edytr1997@gmail.com>
2020-04-04 11:58:37 -0600

    Fix authorization and review comments

--- app/policies/student_policy.rb
+++ app/policies/student_policy.rb
@@ -1,5 +1,14 @@
 class StudentPolicy < ApplicationPolicy
   def show?
+    is_mine_or_admin?
+  end
+
+  def history?
+    is_mine_or_admin?
+  end
+
+  private
+  def is_mine_or_admin?
     return true if @user.is_admin_or_super_admin?
 
     @record.id == @user.student.id
