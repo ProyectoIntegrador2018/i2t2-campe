f8dd8bfc76f6f0fe7b7c3d8b2401ca7f8fdf84b9
Eduardo Trujillo <edytr1997@gmail.com>
2020-04-24 12:06:53 -0500

    Former student education (#129)
    * Add EducationHistory Model
    
    * Add brief description to the migration

--- app/models/curriculum.rb
+++ app/models/curriculum.rb
@@ -1,4 +1,7 @@
 class Curriculum < ApplicationRecord
   belongs_to :student
   has_one_attached :profile_picture
+  has_many :education_histories
+
+  accepts_nested_attributes_for :education_histories
 end
