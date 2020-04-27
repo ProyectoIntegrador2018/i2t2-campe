09c70ef1cf98bebd55a1ddfbf01f2c47908de4a5
Eduardo Trujillo <edytr1997@gmail.com>
2020-04-27 17:49:56 -0500

    Finish controller and authorization policies

--- app/controllers/curriculums_controller.rb
+++ app/controllers/curriculums_controller.rb
@@ -1,22 +1,38 @@
 class CurriculumsController < ApplicationController
+  before_action :set_curriculum, only: [:show, :edit, :update]
+  before_action :authorize_curriculum, only: [:edit, :update, :show]
+
   def new
+    @curriculum = Curriculum.new
   end
 
   def create
+    @curriculum = Curriculum.new(curriculum_params)
+    @curriculum.save!
+    redirect_to curriculum_path(@curriculum)
   end
 
   def update
+    @curriculum.update!(curriculum_params)
+    redirect_to curriculum_path(@curriculum)
   end
 
   def edit
   end
 
-  def destroy
-  end
-
-  def index
-  end
-
   def show
+  end
+
+  private
+  def set_curriculum
+    @curriculum = Curriculum.find(params[:id])
+  end
+
+  def curriculum_params
+    params.require(:curriculum).permit()
+  end
+
+  def authorize_curriculum
+    authorize @curriculum
   end
 end
---
+++ app/policies/curriculum_policy.rb
@@ -0,0 +1,16 @@
+class StudentPolicy < ApplicationPolicy
+  def show?
+    is_mine_or_admin?
+  end
+
+  def update?
+    is_mine_or_admin?
+  end
+
+  private
+  def is_mine_or_admin?
+    return true if @user.is_admin_or_super_admin?
+
+    @record.id == @user.student.id
+  end
+end
