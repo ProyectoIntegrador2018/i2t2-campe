cba5da2d43a1e00e91ad023ac5e1e14266786c6d
Luis Villarreal <ldvo9606@gmail.com>
2020-05-04 20:40:19 -0500

    Optimize js

--- public/javascript/curriculum.js
+++ public/javascript/curriculum.js
@@ -1,12 +1,25 @@
-$(document).bind("DOMSubtreeModified", function() {
-    $("input[id$='is_current']").click(function() {
-        var isCurrent = $(this).prop('checked')
-        var endDateId = $(this).attr('id').replace("is_current", "end_date")
-        var endDateField = $('#' + endDateId).first()
-        if(isCurrent) {
-            endDateField.prop('disabled', true)
-        } else {
-            endDateField.removeAttr("disabled")
-        }
-    });
-});
+$(document).ready(() => {
+    initFields()
+    const observer = new MutationObserver(initFields)
+    observer.observe(document.getElementById("work-experiences"), { childList: true })
+})
+
+function initFields() {
+    $("input[id$='is_current']").each((index, field) => {
+        updateDateFieldDisplay(field)
+        $(field).click((event) => {
+            updateDateFieldDisplay(event.target)
+        })
+    })
+}
+
+function updateDateFieldDisplay(checkboxField) {
+    const isCurrent = $(checkboxField).prop('checked')
+    const endDateId = $(checkboxField).attr('id').replace("is_current", "end_date")
+    const endDateField = $('#' + endDateId).first()
+    if(isCurrent) {
+        endDateField.prop('disabled', true)
+    } else {
+        endDateField.removeAttr("disabled")
+    }
+}
