285f36d30a9ef92a9a8d0f88cd2cff00c2ca1ca7
René Sáenz <rene.saaenz@gmail.com>
2020-05-18 18:38:34 -0500

    Company name filter (#156)
    * Adds filtering JobPosting by Company name
    
    * Enhances filter ui
    
    * Aligns items in filter box

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,8 @@ class Company < ApplicationRecord
   belongs_to :user
   has_many :job_postings
   has_one_attached :logo
+
+  def self.options_for_select
+    order("LOWER(name)").map { |el| [el.name, el.id] }
+  end
 end
--- app/models/job_posting.rb
+++ app/models/job_posting.rb
@@ -3,10 +3,12 @@ class JobPosting < ApplicationRecord
 
   filterrific(
     available_filters: [
-      :search_name
+      :search_name,
+      :with_company_id
     ]
   )
 
   scope :search_name, -> (query_name) { where("title LIKE ?", "%#{query_name}%") }
+  scope :with_company_id, -> (company_id) { where("company_id = ?", company_id) }
   scope :not_expired, -> { where("expiration > ?", Time.now) }
 end
