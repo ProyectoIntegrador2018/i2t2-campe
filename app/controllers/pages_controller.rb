a1506a0430697ecbb4e80d864bb61a8e4142747d
ldvo <14080847+ldvo@users.noreply.github.com>
2020-05-20 14:29:17 -0500

    UI fixes for sign in, side nav and admin tables (#159)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,14 @@
 class PagesController < ApplicationController
   def home
+    if current_user.nil?
+      redirect_to new_user_session_path
+    else
+      if current_user.is_admin_or_super_admin?
+        redirect_to pages_path
+      else
+        redirect_to student_path(current_user.student.id)
+      end
+    end
   end
 
   def index
